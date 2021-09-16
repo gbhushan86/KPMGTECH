9c0fc21d7105b0a2e8a03903afd64e46a5043280
gbhushan86 <34858116+gbhushan86@users.noreply.github.com>
2021-09-16 20:19:56 +0530

    Update main.tf

--- azure/modules/securitygroup/main.tf
+++ azure/modules/securitygroup/main.tf
@@ -1,3 +1,36 @@
+resource "azurerm_network_security_group" "bastion-nsg" {
+    name = "bastion-nsg"
+    location = var.location
+    resource_group_name = var.resource_group
+
+    security_rule {
+        name = "ssh-rule-1"
+        priority = 1001
+        direction = "Inbound"
+        access = "Allow"
+        protocol = "Tcp"
+        source_address_prefix = "192.168.0.0/24"
+        source_port_range = "*"
+        destination_address_prefix = "*"
+        destination_port_range = "22"
+    }
+    
+    security_rule {
+        name = "ssh-rule-2"
+        priority = 1002
+        direction = "Outbound"
+        access = "Allow"
+        protocol = "Tcp"
+        source_address_prefix = "192.168.0.0/24"
+        source_port_range = "*"
+        destination_address_prefix = "*"
+        destination_port_range = "22"
+    }
+
+resource "azurerm_subnet_network_security_group_association" "bastion-nsg-subnet" {
+  subnet_id                 = var.bastion_subnet_id
+  network_security_group_id = azurerm_network_security_group.bastion-nsg.id
+}
 resource "azurerm_network_security_group" "web-nsg" {
   name                = "web-nsg"
   location            = var.location
